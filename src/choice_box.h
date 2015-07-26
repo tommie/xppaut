ace01b2bf30c418aa0aee32933e5bef2eee830c2
Tommie Gannert <tommie@gannert.se>
2015-07-26 22:42:44 +0100

    Minimize choice_box.c API.

--- src/choice_box.h
+++ src/choice_box.h
@@ -19,11 +19,6 @@ typedef struct {
 } CHOICE_BOX;
 
 /* --- Functions --- */
-void destroy_choice(CHOICE_BOX p);
 void display_choice(Window w, CHOICE_BOX p);
-void do_checks(CHOICE_BOX p);
-void base_choice(char *wname, int n, int mcc, char **names, int *check, int type);
-int do_choice_box(Window root, char *wname, int n, int mcc, char **names, int *check, int type);
-int choice_box_event_loop(CHOICE_BOX p);
 
 #endif /* XPPAUT_CHOICE_BOX_H */

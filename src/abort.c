cb36bd4a3935379102e061459456b24e8c7405c6
Tommie Gannert <tommie@gannert.se>
2015-07-26 23:16:01 +0100

    Modernize and clang-format abort.[ch].

--- src/abort.c
+++ src/abort.c
@@ -10,50 +10,46 @@
 #include "main.h"
 #include "many_pops.h"
 
-
-int get_command_width()
-{
-  int x,y;
-  unsigned int w,h,bw,de;
+int get_command_width(void) {
+  int x, y;
+  unsigned int w, h, bw, de;
 
   Window root;
-  XGetGeometry(display,command_pop,&root,&x,&y,&w,&h,&bw,&de);
-  XClearWindow(display,command_pop);
-  return(w);
+  XGetGeometry(display, command_pop, &root, &x, &y, &w, &h, &bw, &de);
+  XClearWindow(display, command_pop);
+  return (w);
 }
 
-void plot_command(nit,icount,cwidth)
-     int nit,icount,cwidth;
-{
+void plot_command(int nit, int icount, int cwidth) {
   int i;
   float dx;
-  if(nit==0)return;
-  dx=(float)icount*(float)cwidth/(float)nit;
-  i=(int)dx;
+  if (nit == 0)
+    return;
+  dx = (float)icount * (float)cwidth / (float)nit;
+  i = (int)dx;
 
-  XDrawPoint(display,command_pop,gc,i,5);
+  XDrawPoint(display, command_pop, gc, i, 5);
 }
 
-
-int my_abort()
-{
+int my_abort(void) {
   int ch;
   XEvent event;
-  while(XPending(display)>0){
-    XNextEvent(display,&event);
-    if(check_ani_pause(event)==27)return(27);
-    switch(event.type){
-    case Expose: do_expose(event);
+  while (XPending(display) > 0) {
+    XNextEvent(display, &event);
+    if (check_ani_pause(event) == 27)
+      return (27);
+    switch (event.type) {
+    case Expose:
+      do_expose(event);
       break;
     case ButtonPress:
       break;
     case KeyPress:
-      ch=get_key_press(&event);
-      return(ch);
-
+      ch = get_key_press(&event);
+      return (ch);
     }
-    return(0);
+    return (0);
   }
 
-  return(64);
+  return (64);
 }

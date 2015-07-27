f6d10380d3e63270b9e80aaef26ffc735cd870ed
Tommie Gannert <tommie@gannert.se>
2015-07-27 21:07:54 +0100

    Modernize and clang-format array_print.[ch].

--- src/array_print.h
+++ src/array_print.h
@@ -2,6 +2,9 @@
 #define XPPAUT_ARRAY_PRINT_H
 
 /* --- Functions --- */
-int array_print(char *filename, char *xtitle, char *ytitle, char *bottom, int nacross, int ndown, int col0, int row0, int nskip, int ncskip, int maxrow, int maxcol, float **data, double zmin, double zmax, double tlo, double thi, int type);
+int array_print(char *filename, char *xtitle, char *ytitle, char *bottom,
+                int nacross, int ndown, int col0, int row0, int nskip,
+                int ncskip, int maxrow, int maxcol, float **data, double zmin,
+                double zmax, double tlo, double thi, int type);
 
 #endif /* XPPAUT_ARRAY_PRINT_H */

91cae716eb9c52ec88582815b576bce776f057f0
Tommie Gannert <tommie@gannert.se>
2015-07-30 08:34:26 +0100

    Modernize and clang-format homsup.[ch].

--- src/homsup.h
+++ src/homsup.h
@@ -3,23 +3,25 @@
 
 /* --- Types --- */
 typedef struct {
-  int nunstab,nstab,n; /* dimension of unstable, stable, phasespace */
-  int eleft,eright; /* addresses of variables holding left and right
-                       equilibria */
-  int u0; /* address of first phase variable */
-  double cprev[1600],a[400];
+  /* dimension of unstable, stable, phasespace */
+  int nunstab, nstab, n;
+  /* addresses of variables holding left and right equilibria */
+  int eleft, eright;
+  /* address of first phase variable */
+  int u0;
+  double cprev[1600], a[400];
   int iflag[4];
-  double fb[400]; /* values of the boundary projections
-                     first nstab are proj to unstable mfld
-                     at left ane then the proj to the stabl
-		     mfld on the right */
+
+  /* values of the boundary projections first nstab are proj to unstable mfld
+     at left ane then the proj to the stabl mfld on the right */
+  double fb[400];
 } HOMOCLIN;
 
 /* --- Data --- */
 extern HOMOCLIN my_hom;
 
 /* --- Functions --- */
-void do_projection(double *x0,double t0,double *x1,double t1);
+void do_projection(double *x0, double t0, double *x1, double t1);
 double hom_bcs(double x);
 
 #endif /* XPPAUT_HOMSUP_H */

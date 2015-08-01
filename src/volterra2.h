970d5666f3dde2a5bd089d8619da4862b3a5cb85
Tommie Gannert <tommie@gannert.se>
2015-08-01 14:54:51 +0100

    Modernize and clang-format volterra2.[ch].

--- src/volterra2.h
+++ src/volterra2.h
@@ -10,6 +10,7 @@ void alloc_v_memory(void);
 void allocate_volterra(int npts, int flag);
 double ker_val(int in);
 void re_evaluate_kernels(void);
-int volterra(double *y, double *t, double dt, int nt, int neq, int *istart, double *work);
+int volterra(double *y, double *t, double dt, int nt, int neq, int *istart,
+             double *work);
 
 #endif /* XPPAUT_VOLTERRA2_H */

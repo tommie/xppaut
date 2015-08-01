9a7b620e2d8cde9b4a721a6408989fb6a66d0e40
Tommie Gannert <tommie@gannert.se>
2015-08-01 14:22:27 +0100

    Modernize and clang-format stiff.[ch].

--- src/stiff.h
+++ src/stiff.h
@@ -1,10 +1,12 @@
-#ifndef _stiff_h_
-#define _stiff_h_
+#ifndef XPPAUT_STIFF_H
+#define XPPAUT_STIFF_H
 
+/* --- Functions --- */
+int adaptive(double *ystart, int nvar, double *xs, double x2, double eps,
+             double *hguess, double hmin, double *work, int *ier, double epjac,
+             int iflag, int *jstart);
+int gadaptive(double *ystart, int nvar, double *xs, double x2, double eps,
+              double *hguess, double hmin, double *work, int *ier, double epjac,
+              int iflag, int *jstart);
 
-/* stiff.c */
-int adaptive(double *ystart, int nvar, double *xs, double x2, double eps, double *hguess, double hmin, double *work, int *ier, double epjac, int iflag, int *jstart);
-int gadaptive(double *ystart, int nvar, double *xs, double x2, double eps, double *hguess, double hmin, double *work, int *ier, double epjac, int iflag, int *jstart);
-
-
-#endif
+#endif /* XPPAUT_STIFF_H */

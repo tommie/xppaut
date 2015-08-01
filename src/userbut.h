6b947d043d8c3b04eaada9286645ac16e1360ce0
Tommie Gannert <tommie@gannert.se>
2015-08-01 14:47:26 +0100

    Modernize and clang-format userbut.[ch].

--- src/userbut.h
+++ src/userbut.h
@@ -1,18 +1,18 @@
-#ifndef _userbut_h_
-#define _userbut_h_
+#ifndef XPPAUT_USERBUT_H
+#define XPPAUT_USERBUT_H
 
 #include <X11/Xlib.h>
 
+/* --- Types --- */
 typedef struct {
   Window w;
   char bname[10];
   int com;
 } USERBUT;
 
+/* --- Functions --- */
 void add_user_button(char *s);
 void create_user_buttons(int x0, int y0, Window base);
 void user_button_events(XEvent report);
 
-
-
-#endif
+#endif /* XPPAUT_USERBUT_H */

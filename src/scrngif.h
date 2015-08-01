f829215540ff0f961d97f5c2ba92a697c34caa19
Tommie Gannert <tommie@gannert.se>
2015-08-01 14:08:36 +0100

    Modernize and clang-format scrngif.[ch].

--- src/scrngif.h
+++ src/scrngif.h
@@ -1,22 +1,20 @@
-#ifndef _scrngif_h_
-#define _scrngif_h_
+#ifndef XPPAUT_SCRNGIF_H
+#define XPPAUT_SCRNGIF_H
 
 #include <stdio.h>
 #include <X11/Xlib.h>
 
- typedef struct GifTree {
-   char typ;             /* terminating, lookup, or search */
-   int code;             /* the code to be output */
-   unsigned char ix;     /* the color map index */
-   struct GifTree **node, *nxt, *alt;
- } GifTree;
+/* --- Types --- */
+typedef struct GifTree {
+  char typ;         /* terminating, lookup, or search */
+  int code;         /* the code to be output */
+  unsigned char ix; /* the color map index */
+  struct GifTree **node, *nxt, *alt;
+} GifTree;
 
+typedef struct { unsigned char r, g, b; } GIFCOL;
 
-typedef struct {
-  unsigned char r,g,b;
-} GIFCOL;
-
-
+/* --- Functions --- */
 void add_ani_gif(Window win, FILE *fp, int count);
 unsigned char *AddCodeToBuffer(int code, short n, unsigned char *buf);
 void end_ani_gif(FILE *fp);
@@ -24,4 +22,4 @@ void get_global_colormap(Window win);
 void screen_to_gif(Window win, FILE *fp);
 void set_global_map(int flag);
 
-#endif
+#endif /* XPPAUT_SCRNGIF_H */

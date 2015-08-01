89285a95074bc371b5ae00b1068c43cbdb3e29db
Tommie Gannert <tommie@gannert.se>
2015-08-01 14:14:24 +0100

    Modernize and clang-format simplenet.[ch].

--- src/simplenet.h
+++ src/simplenet.h
@@ -1,6 +1,7 @@
-#ifndef _simplenet_h_
-#define _simplenet_h_
+#ifndef XPPAUT_SIMPLENET_H
+#define XPPAUT_SIMPLENET_H
 
+/* --- Functions --- */
 int add_spec_fun(char *name, char *rhs);
 void add_special_name(char *name, char *rhs);
 void eval_all_nets(void);
@@ -8,5 +9,4 @@ int g_namelist(char *s, char *root, int *flag, int *i1, int *i2);
 double network_value(double x, int i);
 void update_all_ffts(void);
 
-
-#endif
+#endif /* XPPAUT_SIMPLENET_H */

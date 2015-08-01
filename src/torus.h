9db282fea9e02c812988f8ad2a24e45924f54d98
Tommie Gannert <tommie@gannert.se>
2015-08-01 14:37:35 +0100

    Modernize and clang-format torus.[ch].

--- src/torus.h
+++ src/torus.h
@@ -1,11 +1,7 @@
-#ifndef _torus_h_
-#define _torus_h_
+#ifndef XPPAUT_TORUS_H
+#define XPPAUT_TORUS_H
 
-#include <X11/Xlib.h>
-
+/* --- Functions --- */
 void do_torus_com(int c);
 
-
-
-
-#endif
+#endif /* XPPAUT_TORUS_H */

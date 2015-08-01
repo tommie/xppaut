42e70bb61fe5a91841c0e35832ec85f2b4361d99
Tommie Gannert <tommie@gannert.se>
2015-08-01 14:26:29 +0100

    Modernize and clang-format storage.[ch].

--- src/storage.h
+++ src/storage.h
@@ -12,6 +12,6 @@ extern double *WORK;
 void alloc_meth(void);
 void init_alloc_info(void);
 void init_stor(int nrow, int ncol);
-int reallocstor(int ncol,int nrow);
+int reallocstor(int ncol, int nrow);
 
 #endif /* XPPAUT_STORAGE_H */

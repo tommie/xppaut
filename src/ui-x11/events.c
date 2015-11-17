b45805dc0aa52ac3aa75d555c9f070243e201882
Tommie Gannert <tommie@gannert.se>
2015-11-17 17:41:02 +0000

    Fix x11_events_unlisten.

--- src/ui-x11/events.c
+++ src/ui-x11/events.c
@@ -102,6 +102,8 @@ int x11_events_dispatch(X11Events *evs, const XEvent *ev) {
   for (size_t i = 0; i < evs->cbs_cache.len; ++i) {
     const X11EventsCb *cb = &evs->cbs_cache.elems[i];
 
+    // TODO: Check that cb still exists in cbs.
+
     if ((cb->window == X11_EVENTS_ANY_WINDOW ||
          ev->xany.window == cb->window) &&
         (mask & cb->event_mask)) {
@@ -128,6 +130,8 @@ int x11_events_run(X11Events *evs) {
 
 int x11_events_listen(X11Events *evs, Window w, long mask, X11EventsFunc func,
                       void *cookie) {
+  long sel_mask = mask;
+
   // Check if we already have something registered.
   for (size_t i = 0; i < evs->cbs.len; ++i) {
     const X11EventsCb *cb = &evs->cbs.elems[i];
@@ -135,13 +139,15 @@ int x11_events_listen(X11Events *evs, Window w, long mask, X11EventsFunc func,
     if (w != X11_EVENTS_ANY_WINDOW && cb->window != w)
       continue;
 
-    if (cb->event_mask == mask && cb->func == func && cb->cookie == cookie)
+    if (cb->window == w && cb->event_mask == mask && cb->func == func &&
+        cb->cookie == cookie)
       return 1;
 
-    mask |= cb->event_mask;
+    if (cb->window == w)
+      sel_mask |= cb->event_mask;
   }
 
-  if (w != X11_EVENTS_ANY_WINDOW && XSelectInput(evs->display, w, mask))
+  if (w != X11_EVENTS_ANY_WINDOW && XSelectInput(evs->display, w, sel_mask))
     return 1;
 
   X11EventsCb *cb = x11_events_cbs_append(&evs->cbs);
@@ -166,8 +172,11 @@ int x11_events_unlisten(X11Events *evs, Window w, long mask, X11EventsFunc func,
 
     x11_events_cbs_remove(&evs->cbs, i, 1);
     // TODO: Do XSelectInput if needed for performance.
-    break;
+    return 0;
   }
 
-  return 0;
+  fprintf(stderr,
+          "x11_events_unlisten: no match for w=0x%lX, mask=0x%lX, cookie=%p\n",
+          w, mask, cookie);
+  return 1;
 }
